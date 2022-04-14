49ad6e277cf386dbaf5413390da50dfa328d7d65
atdav3 <davetranxx@gmail.com>
2022-04-14 10:00:00 +0000

    feat: enhance navigation component with scroll detection and improved styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaSun, FaMoon } from "react-icons/fa6";
 
 export default function Navigation({ theme, setTheme, activeSection, scrollToSection }) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
     return (
-        <nav className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+        <nav className={`fixed top-4 left-4 right-4 z-50 transition-all duration-500 ${
             theme === 'dark' 
-                ? 'bg-gray-950/80 backdrop-blur-md border-gray-800/50' 
-                : 'bg-white/80 backdrop-blur-md border-gray-200/50'
-        } border-b`}>
+                ? 'bg-gray-950/20 backdrop-blur-md border-gray-800/20' 
+                : 'bg-white/20 backdrop-blur-md border-gray-200/20'
+        } border rounded-2xl shadow-lg`}>
             <div className="max-w-7xl mx-auto px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     {/* Logo */}
                     <div className="flex-shrink-0">
                         <button
                             onClick={() => scrollToSection('hero')}
-                            className={`text-2xl font-bold transition-colors ${
-                                theme === 'dark' ? 'text-white hover:text-blue-400' : 'text-gray-900 hover:text-blue-600'
-                            }`}
+                            className="text-2xl font-bold transition-all duration-300 hover:scale-105"
                             style={{
                                 fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, sans-serif'
                             }}
                         >
-                            VC
-                            <span className="inline-block ml-1" style={{
+                            <span className={`bg-gradient-to-r ${
+                                theme === 'dark' 
+                                    ? 'from-cyan-400 via-blue-500 to-purple-600' 
+                                    : 'from-cyan-600 via-blue-700 to-purple-800'
+                            } bg-clip-text text-transparent`}>
+                                vietcq
+                            </span>
+                            <span className={`inline-block ml-1 ${
+                                theme === 'dark' ? 'text-purple-400' : 'text-purple-600'
+                            }`} style={{
                                 animation: 'float 3s ease-in-out infinite'
                             }}>.</span>
                         </button>
@@ -43,20 +59,14 @@ export default function Navigation({ theme, setTheme, activeSection, scrollToSec
                                 <button
                                     key={item.id}
                                     onClick={() => scrollToSection(item.id)}
-                                    className={`px-3 py-2 text-sm font-medium transition-all duration-200 relative ${
+                                                                                                    className={`px-4 py-3 rounded-lg transition-colors text-base font-medium ${
                                         activeSection === item.id
-                                            ? (theme === 'dark' ? 'text-blue-400' : 'text-blue-600')
-                                            : (theme === 'dark' ? 'text-gray-300 hover:text-white' : 'text-gray-600 hover:text-gray-900')
+                                            ? "text-[#2365E3] dark:text-[#ff6100] bg-[#2365E3]/10 dark:bg-[#ff6100]/10"
+                                            : "text-gray-600 dark:text-gray-300 hover:text-[#2365E3] dark:hover:text-[#ff6100] hover:bg-gray-50 dark:hover:bg-gray-800/50"
                                     }`}
                                 >
                                     {item.label}
-                                    {activeSection === item.id && (
-                                        <span className={`absolute bottom-0 left-0 right-0 h-0.5 ${
-                                            theme === 'dark' ? 'bg-blue-400' : 'bg-blue-600'
-                                        }`} style={{
-                                            animation: 'slideIn 0.3s ease-out'
-                                        }} />
-                                    )}
+
                                 </button>
                             ))}
                         </div>
@@ -66,11 +76,7 @@ export default function Navigation({ theme, setTheme, activeSection, scrollToSec
                     <div className="flex items-center space-x-4">
                         <button
                             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                            className={`p-2 rounded-lg transition-all duration-200 ${
-                                theme === 'dark' 
-                                    ? 'text-gray-400 hover:text-white hover:bg-gray-800' 
-                                    : 'text-gray-600 hover:text-gray-900 hover:bg-gray-100'
-                            }`}
+                            className="w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center flex-shrink-0 rounded-lg hover:bg-black/5 dark:hover:bg-white/5 transition-colors"
                         >
                             {theme === 'dark' ? <FaSun size={18} /> : <FaMoon size={18} />}
                         </button>
@@ -78,11 +84,7 @@ export default function Navigation({ theme, setTheme, activeSection, scrollToSec
                         {/* Mobile menu button */}
                         <button
                             onClick={() => setIsMenuOpen(!isMenuOpen)}
-                            className={`md:hidden p-2 rounded-lg transition-all duration-200 ${
-                                theme === 'dark' 
-                                    ? 'text-gray-400 hover:text-white hover:bg-gray-800' 
-                                    : 'text-gray-600 hover:text-gray-900 hover:bg-gray-100'
-                            }`}
+                            className="md:hidden w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center flex-shrink-0 rounded-lg hover:bg-black/5 dark:hover:bg-white/5 transition-colors"
                         >
                             <div className="w-5 h-5 flex flex-col justify-around">
                                 <span className={`block h-0.5 w-full transition-all duration-300 ${
@@ -106,7 +108,7 @@ export default function Navigation({ theme, setTheme, activeSection, scrollToSec
                     } backdrop-blur-md border-t ${
                         theme === 'dark' ? 'border-gray-800/50' : 'border-gray-200/50'
                     }`}>
-                        <div className="px-2 pt-2 pb-3 space-y-1">
+                        <nav className="container mx-auto px-4 py-6 flex flex-col space-y-2">
                             {[
                                 { id: 'hero', label: 'Home' },
                                 { id: 'about', label: 'About' },
@@ -120,16 +122,16 @@ export default function Navigation({ theme, setTheme, activeSection, scrollToSec
                                         scrollToSection(item.id);
                                         setIsMenuOpen(false);
                                     }}
-                                    className={`block w-full text-left px-3 py-2 text-base font-medium transition-all duration-200 ${
+                                    className={`block px-4 py-3 rounded-lg transition-colors text-base font-medium ${
                                         activeSection === item.id
-                                            ? (theme === 'dark' ? 'text-blue-400 bg-gray-800/50' : 'text-blue-600 bg-blue-50')
-                                            : (theme === 'dark' ? 'text-gray-300 hover:text-white hover:bg-gray-800/50' : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50')
+                                            ? "text-[#2365E3] dark:text-[#ff6100] bg-[#2365E3]/10 dark:bg-[#ff6100]/10"
+                                            : "text-gray-600 dark:text-gray-300 hover:text-[#2365E3] dark:hover:text-[#ff6100] hover:bg-gray-50 dark:hover:bg-gray-800/50"
                                     }`}
                                 >
-                                    {item.label}
+                                    <strong>{item.label}</strong>
                                 </button>
                             ))}
-                        </div>
+                        </nav>
                     </div>
                 )}
             </div>
